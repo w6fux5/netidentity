18aa25f0ddb08cf028c724f50824c18e29353a49
mike <w6fux5@gmail.com>
2023-04-16 14:02:30 +0800

    Add project files.

---
+++ IdentityManager/Data/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityManager.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext() : base()
+    {
+
+    }
+
+
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+}
